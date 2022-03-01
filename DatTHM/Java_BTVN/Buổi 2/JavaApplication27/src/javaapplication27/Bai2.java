81c81c897d19a66cd9560d57e5e2a066f0e570d2
Đạt <tranhuydat.gl@gmail.com>
2022-03-02 00:08:41 +0700

    sua bai2 java

--- DatTHM/Java_BTVN/Buổi 2/JavaApplication27/src/javaapplication27/Bai2.java
+++ DatTHM/Java_BTVN/Buổi 2/JavaApplication27/src/javaapplication27/Bai2.java
@@ -9,6 +9,7 @@ package javaapplication27;
  * @author admin
  */
 public class Bai2 {
+
     public static void main(String[] args) {
         String str = "1aba1bh11 ";
         int mid = str.length() / 2;
@@ -19,18 +20,26 @@ public class Bai2 {
             next += 1;
         }
         char arr[] = str.toCharArray();
+//        for (int i = 0; i < mid; i++) {
+//            
+//            for (int j = next; j < str.length() - 1;) {
+//                char x = str.charAt(arr[i]);
+//                char y = str.charAt(arr[arr.length-1-i]);
+//                int a = Character.compare(x, y);
+//                if (a == 0) {
+//                    System.out.println("i= " + str.charAt(arr[i]));
+//                }
+//                j++;
+//                break;
+//            }
+//        }
         for (int i = 0; i < mid; i++) {
-            
-            for (int j = next; j < str.length() - 1;) {
-                char x = str.charAt(arr[i]);
-                char y = str.charAt(arr[arr.length-1-i]);
-                int a = Character.compare(x, y);
-                if (a == 0) {
-                    System.out.println("i= " + str.charAt(arr[i]));
-                }
-                j++;
+            char a = str.charAt(i);
+            char b = str.charAt(str.length() - 1 - i);
+            while (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+                System.out.println("i= " + str.charAt(i));
                 break;
             }
         }
     }
-}
+    }
